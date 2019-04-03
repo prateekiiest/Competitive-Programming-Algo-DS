f15e79d60f7c5fb0ccdd8a9f87f2acd6056cdef2
Prateek Chanda <prateekkol21@gmail.com>
2019-04-03 20:17:27 +0530

    Create multiples_3_and_5.py

---
+++ Hackerrank/Algorithms/Project-Euler/multiples_3_and_5.py
@@ -0,0 +1,12 @@
+# Problem Statement : https://www.hackerrank.com/contests/projecteuler/challenges/euler001
+
+t=int(input())
+def ar(x):
+    return x*(x+1);
+for i in range(t):
+    n =int(input())
+    n -=1;
+    a=int(n/3);
+    b=int(n/5);
+    c=int(n/15);
+    print(int(int(3*ar(a) + 5*ar(b) - 15*ar(c))>>1));
