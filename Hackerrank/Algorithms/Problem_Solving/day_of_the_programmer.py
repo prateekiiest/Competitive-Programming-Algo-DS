3b740a744f691cd3b8210cd2f06c48aff2d4cabf
Prateek Chanda <prateekkol21@gmail.com>
2019-04-03 15:28:11 +0530

    Create day_of_the_programmer.py

---
+++ Hackerrank/Algorithms/Problem_Solving/day_of_the_programmer.py
@@ -0,0 +1,17 @@
+# Problem Statement : https://www.hackerrank.com/challenges/day-of-the-programmer
+
+#!/bin/python
+
+import sys
+def solve(year):
+    if (year == 1918):
+        return '26.09.1918'
+    elif ((year <= 1917) & (year%4 == 0)) or ((year > 1918) & (year%400 == 0 or ((year%4 == 0) & (year%100 != 0)))):
+        return '12.09.%s' %year
+    else:
+        return '13.09.%s' %year
+    
+    
+year = int(raw_input().strip())
+result = solve(year)
+print(result)
