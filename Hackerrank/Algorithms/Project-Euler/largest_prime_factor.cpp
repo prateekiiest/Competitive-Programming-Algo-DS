633c246b763a5a50ecd524662e50857464421c2a
Prateek Chanda <prateekkol21@gmail.com>
2019-04-03 20:27:09 +0530

    Create largest_prime_factor.cpp

---
+++ Hackerrank/Algorithms/Project-Euler/largest_prime_factor.cpp
@@ -0,0 +1,30 @@
+// Problem Statement : https://www.hackerrank.com/contests/projecteuler/challenges/euler003
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+
+ll solve(ll n){
+    ll b = n;
+    for(ll i=2;;i++){
+        if(i*i > b){
+            return b;
+        }
+        if(n%i == 0){
+            return max(i, solve(n/i));
+        }
+    }
+}
+int main() {
+    int t;cin>>t;
+    while(t--){
+        ll n; cin >> n;
+        ll m = solve(n);
+        cout<<m<<endl;
+    }
+    return 0;
+}
