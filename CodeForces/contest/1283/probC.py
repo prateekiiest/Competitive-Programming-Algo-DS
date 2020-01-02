8f7b035f8f717d5ba316aee206da316e68f2b8dc
Prateek Chanda <prateekkol21@gmail.com>
2020-01-02 21:51:47 +0530

    Partially complete 1283 contest

--- CodeForces/contest/1283/probC.py
+++ CodeForces/contest/1283/probC.py
@@ -1,12 +1,20 @@
-n = int(input())
-
-
-arr = raw_input().split()
-arr = [int(k) for k in arr]
-
-output = [0 for i in range(n)]
-
-for item in range(n):
-    if arr[item] != 0:
-        output[item] = arr[item] 
-print(output)
+N = input()
+A = map(int, raw_input().split())
+ 
+V = [i + 1 for i in xrange(N) if A[i] == 0]
+f = [1] * (N + 1)
+for i in A: f[i] = 0
+F = [i for i in range(1, N + 1) if f[i]]
+ 
+n = len(F)
+ 
+from random import shuffle as s
+while 1:
+    s(F)
+    t = [i for i in xrange(n) if F[i] == V[i]]
+    if not t:
+        break
+    
+for i in xrange(n):
+    A[V[i] - 1] = F[i]
+for i in A: print i,
